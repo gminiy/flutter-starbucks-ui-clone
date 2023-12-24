1719a4e5cff83433f5da2ad2e7209eac1297934e
gminiy <gmini.y@gmail.com>
2023-12-25 01:00:57 +0900

    feat: star point star count 구현

--- lib/widget/star_point.dart
+++ lib/widget/star_point.dart
@@ -5,39 +5,69 @@ class StarPoint extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child:  Row(
-          children: [
-            Flexible(
-              flex: 2,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 8.0),
-                      child: Text('9 \u{2605} untile Gold Level', style: TextStyle(color: Colors.green, fontSize: 15, fontWeight: FontWeight.bold)),
-                    ),
-                    LinearProgressIndicator(
-                      value: 0.6,
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.green,
-                      minHeight: 8,
-                      semanticsLabel: 'Linear progress indicator',
-                    ),
-                  ],
-                ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8.0),
+                    child: Text('9 \u{2605} untile Gold Level',
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  LinearProgressIndicator(
+                    value: 0.6,
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.green,
+                    minHeight: 8,
+                    semanticsLabel: 'Linear progress indicator',
+                  ),
+                ],
               ),
             ),
-            Flexible(
-              flex: 1,
-              child: Text('text')
-            ),
-          ],
-        ),
-      );
+          ),
+          const Flexible(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '16',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    ' / ',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    '25\u{2605}',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
   }
 }
